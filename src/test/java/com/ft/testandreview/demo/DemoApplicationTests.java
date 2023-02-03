bc163537705c2d7ba290cc08a5810421c62417ee
gaorui915187242 <gao915187242@163.com>
2023-02-03 19:16:56 +0800

    Initial commit

---
+++ src/test/java/com/ft/testandreview/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft.testandreview.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
